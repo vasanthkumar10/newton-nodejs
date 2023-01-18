7ecaee67afc2b34b44150ba30988a5680c9244de
Vasanthkumar <vasizebron10@gmail.com>
2023-01-18 21:35:04 +0530

    node is single threaded synchronous

--- concept.js
+++ concept.js
@@ -20,17 +20,30 @@ const os = require("os");
 // console.log(Date.now() - startSync);
 
 // async way
+const https = require("https");
 const start = Date.now();
 
-console.log("CPU length", os.cpus());
+// console.log("CPU length", os.cpus());
 
-process.env.UV_THREADPOOL_SIZE = 17;
+// process.env.UV_THREADPOOL_SIZE = 17;
 // console.log(process.env);
 
 let MAX_CALLS = 17;
 
 for (let i = 1; i <= MAX_CALLS; i++) {
-  crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
-    console.log(`Iteration ${i} -`, Date.now() - start);
-  });
+  // crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
+  //   console.log(`Iteration ${i} -`, Date.now() - start);
+  // });
+  https
+    .request("https://www.google.com", (res) => {
+      res.on("data", () => {
+        // console.log("got the data");
+      });
+      res.on("end", () => {
+        console.log(`Iteration ${i} -`, Date.now() - start);
+      });
+    })
+    .end();
 }
+
+// libuv
